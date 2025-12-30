d141dbc38482d93c4e46089835f1c9ce69abf658
seelamGayatri2006 <seelamgayatri18@gmail.com>
2025-12-30 21:06:36 +0530

    Initial commit: Flask AI Quote app without env and venv

--- app.py
+++ app.py
@@ -28,21 +28,19 @@ def get_quote():
 
 app = Flask(__name__)
 app.config['SECRETKEY'] = os.getenv("SECRETKEY")
-@app.route('/')
+
+@app.route('/',methods=['GET','POST'])
+def house():
+    return render_template('house.html')
+
+@app.route('/home')
 def home():
     quote,author,category=get_quote()
     return render_template('ai_quote.html',quote1=quote,author1=author, category1=category)
 
 
 @app.route('/login',methods=['GET','POST'])
-# def login():
-#      if request.method== 'POST':
-#         username = request.form.get('username')
-#         password = request.form.get('password')
-#         cursor.execute("SELECT * FROM users WHERE username=%s AND password=%s",(username, password))
-#         user = cursor.fetchone()
-#         return render_template('login.html')
-#      return render_template('login.html')
+
 def login():
      if request.method == 'POST':
         username = request.form.get('username')
@@ -51,7 +49,7 @@ def login():
         user = cursor.fetchone()
 
         if user:
-            return redirect('/')
+            return redirect('/home')
         else:
             return redirect('/login')
 
@@ -59,6 +57,7 @@ def login():
 
 @app.route('/logout',methods=['GET','POST'])
 def logout():
+    # return redirect('/')
     return render_template('logout.html')
 @app.route('/registration',methods=['GET','POST'])
 def registration():
@@ -72,17 +71,11 @@ def registration():
         return redirect('/login')
     return render_template('registration.html')
 
-@app.route('/house',methods=['GET','POST'])
-def house():
-    return render_template('house.html')
+# @app.route('/house',methods=['GET','POST'])
+# def house():
+#     return render_template('house.html')
 
-@app.route('/about',methods=['GET','POST'])
-def about():
-    return "Welcome back to my channel"
 
-@app.route('/info',methods=['GET','POST'])
-def info(): 
-    return render_template('index.html')
 
 app.run(use_reloader=True,debug=True)
 # app.run(debug=True,use_reloader=True) or app.run()
